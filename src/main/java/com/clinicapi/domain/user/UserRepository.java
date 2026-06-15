580c6cf4289fcd2a2a7895a63e41822fb0cae868
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-06-14 22:11:44 -0300

    add: user repository created

---
+++ src/main/java/com/clinicapi/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.clinicapi.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    public UserDetails findByLogin(String login);
+}
