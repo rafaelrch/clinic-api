5afbf21338426cfe2e9eb89c1ae18d9fba7a86d3
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-06-16 14:21:13 -0300

    feat(security): configure Spring Security with JWT filter

--- src/main/java/com/clinicapi/infra/security/SecurityFilter.java
+++ src/main/java/com/clinicapi/infra/security/SecurityFilter.java
@@ -1,4 +1,57 @@
 package com.clinicapi.infra.security;
 
-public class SecurityFilter {
+import com.clinicapi.domain.user.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String tokenJWT = recoverToken(request);
+
+        if(tokenJWT != null){
+            String subject = tokenService.getSubject(tokenJWT); //  Validar o token e devolver o login do dono. Se o token for inválido, dispara exception
+
+            // obs: "adicionar handler pra JWTVerificationException retornando 401"
+
+            UserDetails user = userRepository.findByLogin(subject); // Busca o usuário no banco. Por isso o UserRepository.findByLogin()
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            /* Cria um objeto de autenticação com os seguintes parametros
+                • user -> o principal(quem é)
+                • null -> as credentials(não precisa, ja validamos via JWT)
+                • user.getAuthorities() -> as permissões(["ROLE_ADMIN"], etc)
+             */
+            SecurityContextHolder.getContext().setAuthentication(authentication); // Cola o "post-it". A partir desse momento, o Spring sabe quem é o usuário.
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request){
+        String authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader == null){
+            return null;
+        }
+        return authorizationHeader.replace("Bearer ", "");
+    }
 }
