aa69bbf372c742ed63a5e607a418ec713651a51a
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-06-30 14:33:37 -0300

    test: setup test infrastructure with H2 and Testcontainers dependencies

--- src/test/java/com/clinicapi/clinicapi/ClinicApiApplicationTests.java
+++ src/test/java/com/clinicapi/clinicapi/ClinicApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.clinicapi.clinicapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClinicApiApplicationTests {
 
 	@Test
