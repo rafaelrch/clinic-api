7f4880ab23d9728cd9713d3e9ea448a62a20f6dc
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-07-03 14:10:33 -0300

    test: add DoctorService findById not found scenario

--- src/test/java/com/clinicapi/domain/service/DoctorServiceTest.java
+++ src/test/java/com/clinicapi/domain/service/DoctorServiceTest.java
@@ -4,6 +4,7 @@ import com.clinicapi.domain.doctor.Doctor;
 import com.clinicapi.domain.doctor.DoctorRepository;
 import com.clinicapi.domain.doctor.DoctorResponseData;
 import com.clinicapi.domain.doctor.Specialty;
+import com.clinicapi.domain.service.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,5 +43,22 @@ class DoctorServiceTest {
         assertThat(result.name()).isEqualTo("Dr. Test");
         assertThat(result.email()).isEqualTo("test@email.com");
         assertThat(result.specialty()).isEqualTo(Specialty.CARDIOLOGY);
+
+
     }
+
+    @Test
+    @DisplayName("Should return empty when throw Resource Not Found Exception when doctor does not exist")
+    void shouldThrowResourceNotFoundExceptionWhenDoctorDoesNotExist(){
+
+        //ARRANGE
+        when(doctorRepository.findByIdAndActiveTrue(99L)).thenReturn(Optional.empty());
+
+        // ACT & ASSERT
+        assertThatThrownBy(() -> service.findById(99L))
+                .isInstanceOf(ResourceNotFoundException.class)
+                .hasMessageContaining("99");
+    }
+
+
 }
