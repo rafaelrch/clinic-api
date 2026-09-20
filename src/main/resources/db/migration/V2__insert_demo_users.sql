6bb0d07c8ffe71bad979609480d7b88c3650f0f5
Rafael Rocha <115195984+rafaelrch@users.noreply.github.com>
2026-09-20 00:28:42 -0300

    feat: add Flyway migrations for schema and demo users

---
+++ src/main/resources/db/migration/V2__insert_demo_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (login, password, role) VALUES
+    ('admin@clinicapi.com',   '$2a$10$/.LBD.Cx5NZoEW7nezCkreZZkF8Ep9PXuCtlawrrc/xXvs0VdoGK6', 'ADMIN'),
+    ('doctor@clinicapi.com',  '$2a$10$/.LBD.Cx5NZoEW7nezCkreZZkF8Ep9PXuCtlawrrc/xXvs0VdoGK6', 'DOCTOR'),
+    ('patient@clinicapi.com', '$2a$10$/.LBD.Cx5NZoEW7nezCkreZZkF8Ep9PXuCtlawrrc/xXvs0VdoGK6', 'PATIENT');
